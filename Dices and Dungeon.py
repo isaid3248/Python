9041bb055e7da3ffa2c91dc1f15d790851cf6b41
isaid3248 <isaid3248@gmail.com>
2021-07-08 00:07:57 +0300

    Python

--- Dices and Dungeon.py
+++ Dices and Dungeon.py
@@ -1,42 +1,42 @@
 import random
 import time
 
-can =15
-ccan=30
+can = 15
+ccan = 30
 while can > 0 and ccan > 0:
-    x=input("Saldırı için a,savunma için d: ")
+    x = input("Saldırı için A, savunma için D: ")
     x = x.upper()
-    a = random.randint(1,2)
+    a = random.randint(1, 2)
     if x == "A":
         if a == 1:
-            print("Ataktayız,zarlar atılıyor.")
+            print("Ataktayız, zarlar atılıyor...")
             time.sleep(2)
-            zar = random.randint(1,6)
-            czar = random.randint(1,6)
+            zar = random.randint(1, 6)
+            czar = random.randint(1, 6)
             if zar > czar:
-                print("Canavara üstün geldin")
+                print("Canavara üstün geldin.")
                 guc = zar - czar
-                ccan = ccan-guc
+                ccan = ccan - guc
                 print("""
 Canavarın canı: {}
 Bizim canımız: {}
-""".format(ccan,can))
+""".format(ccan, can))
                 time.sleep(1)
             else:
                 print("Canavar üstün geldi.")
                 cguc = czar - zar
-                can = can - cguc            
+                can = can - cguc
                 print("""
 Canavarın canı: {}
 Bizim canımız: {}
-""".format(ccan,can))
+""".format(ccan, can))
                 time.sleep(1)
         else:
             print("Atağı savuşturdu.")
             print("""
 Canavarın canı: {}
 Bizim canımız: {}
-""".format(ccan,can))
+""".format(ccan, can))
             time.sleep(1)
     else:
         if a == 1:
@@ -44,23 +44,19 @@ Bizim canımız: {}
             print("""
 Canavarın canı: {}
 Bizim canımız: {}
-""".format(ccan,can))
+""".format(ccan, can))
             time.sleep(1)
         else:
-            print("Hiçbir şey olmadı,her iki taraf da defansta")
+            print("Hiçbir şey olmadı, her iki taraf da defansta.")
             print("""
 Canavarın canı: {}
 Bizim canımız: {}
-""".format(ccan,can))
+""".format(ccan, can))
             time.sleep(1)
 
 if ccan < 0:
-    print("Canavarı mağlup ettin")
+    print("Canavarı mağlup ettin.")
     time.sleep(5)
 elif can < 0:
-    print("Canavar tarafından yendin.")
+    print("Canavar tarafından yenildin.")
     time.sleep(5)
-                
-            
-    
-        
