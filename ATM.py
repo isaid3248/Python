90083b9cdcf95183e6e5e1ae247f7b320b3bc465
isaid3248 <isaid3248@gmail.com>
2021-07-07 22:18:06 +0300

    Python

--- ATM.py
+++ ATM.py
@@ -1,5 +1,5 @@
-balance=897.32
-while True:    
+balance = 897.32
+while True:
     print("    ATM    ")
     print("""
     1)        Balance
@@ -9,33 +9,30 @@ while True:
 
 
     """)
-    Option=int(input("Enter Option: "))
+    Option = int(input("Enter Option: "))
 
-    if Option==1:
-        print("Balance  £ ",balance)
+    if Option == 1:
+        print("Balance  £ ", balance)
 
-
-    if Option==2:
-        print("Balance  £  ",balance)
-        Withdraw=float(input("Enter Withdraw amount £ "))
-        if Withdraw>0:
-            balance=(balance-Withdraw)
-            print("Foreward Balance  £ ",balance)
-        elif Withdraw>balance:
+    if Option == 2:
+        print("Balance  £  ", balance)
+        Withdraw = float(input("Enter Withdraw amount £ "))
+        if Withdraw > 0:
+            balance = (balance-Withdraw)
+            print("Foreward Balance  £ ", balance)
+        elif Withdraw > balance:
             print("No funs in account")
         else:
             print("None withdraw made")
 
-    if Option==3:
-        print("Balance  £ ",balance)
-        Deposit=float(input("Enter deposit amount £ "))
-        if Deposit>0:
-            balance=(balance+Deposit)
-            print("Forewardbalance  £ ",balance)
+    if Option == 3:
+        print("Balance  £ ", balance)
+        Deposit = float(input("Enter deposit amount £ "))
+        if Deposit > 0:
+            balance = (balance+Deposit)
+            print("Forewardbalance  £ ", balance)
         else:
             print("None deposit made")
 
-
-    if Option==4:
+    if Option == 4:
         exit()
-
